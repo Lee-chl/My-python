4fd070aa9a90c5e01e735596ed88d86a156912bb
Lee Yeji <53994972+Lee-chl@users.noreply.github.com>
2025-06-28 16:19:02 +0900

    # Update gugudan.py
    if문 추가해 1~9가 아닐 경우 취소하게
    + 숫자 0(종료)가 아닐 경우 계속 반복하게

--- 파이썬/gugudan.py
+++ 파이썬/gugudan.py
@@ -1,7 +1,17 @@
-print("구구단 몇 단을 게산할까요??")
-num = int(input())
-print(f"{num}단을 계산합니다.")
+num = 1
 
-for i in range(1,10):
-    result = num * i
-    print(f"{num} X {i} = {result}")
+while num !=0:
+    print("구구단 몇 단을 게산할까요?? [0 입력 시 종료]")
+    num = int(input())
+
+    if num == 0:
+        print("구구단 계산을 종료합니다.")
+        break
+    elif num < 1 or num > 9: 
+        print("0단 부터 9단까지의 구구단만 계산할수 있습니다.")
+        continue
+
+    print(f"{num}단을 계산합니다.")
+    for i in range(1,10):
+        result = num * i
+        print(f"{num} X {i} = {result}")
